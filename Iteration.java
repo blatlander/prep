825305d97056e256369bcf82f10a74e51d4ac87a
blatlander <blatlander@gmail.com>
2017-06-12 12:20:49 +0700

    Add comment

--- Iteration.java
+++ Iteration.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-
+//test
 public class Iteration{
 	public static void main(String args[]) throws Exception{
 		//int ar[] = {0,0,0,1,1,0,0,1,1,0,1,1,1,1,0,0,0,0,0,0,0,1};
